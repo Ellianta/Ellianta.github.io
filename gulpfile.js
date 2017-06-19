2d7eafa07b70b073ceed05f62ad2b4f37de201ad
Ellianta <metlinova86@mail.ru>
2017-06-19 15:43:20 +0300

    index
    доработка дизайна

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,23 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var browserSync = require("browser-sync");
+var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("style", function() {
-  gulp.src("apps/less/style.less")
+  gulp.src("less/style.less")
   .pipe(plumber())
   .pipe(less())
-  .pipe(gulp.dest("apps/css"))
+  .pipe(autoprefixer({
+      browsers: ["last 2 versions"]
+    }))
+  .pipe(gulp.dest("css"))
   .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task("serve", ["style"], function(){
   browserSync.init({
-    server: "./apps"
+    server: "."
   });
-  gulp.watch("apps/less/**/*.less", ["style"]);
-  gulp.watch("apps/*.html").on("change", browserSync.reload);
+  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("*.html").on("change", browserSync.reload);
 });
